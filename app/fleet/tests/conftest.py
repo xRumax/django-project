b94e2ceb0130362c4c33b3478ae575b3f39cf045
xRumax <167364@student.uwm.edu.pl>
2026-03-13 18:26:51 +0100

    feat: create fixture for sample_car

---
+++ app/fleet/tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest 
+from fleet.models.car import Car 
+
+@pytest.fixture
+def sample_car(db):
+    return Car.objects.create(
+    brand="Tesla",
+    model="Model S",
+    year=2024,
+    hp=670
+)
