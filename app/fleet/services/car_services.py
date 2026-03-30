c6d6c45c085c4046f9ecb7be5d7326da1bd40761
xRumax <167364@student.uwm.edu.pl>
2026-03-30 09:13:39 +0200

    Feat(car): Create date validator

---
+++ app/fleet/services/car_services.py
@@ -0,0 +1,9 @@
+from django.core.exceptions import ValidationError
+from datetime import datetime
+
+def check_car_year(year):
+    if year < 1800:
+        raise ValidationError({'year':"Production car year should be above 1800."})
+    if year > datetime.now().year + 1:
+        raise ValidationError({'year':"Wrong car year production. This is the future date."})
+
