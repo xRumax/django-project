499dc26f368450d058ae3cd8bdd0a7da38556056
xRumax <167364@student.uwm.edu.pl>
2026-04-02 12:27:38 +0200

    Refactor(api): prepare for JWT implementation

--- app/rentals/serializers/rental_serializer.py
+++ app/rentals/serializers/rental_serializer.py
@@ -4,12 +4,13 @@ from django.core.exceptions import ValidationError as DjangoValidationError
 
 class RentalSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)
-
     class Meta:
         model = Rental
         fields = ['car','user', 'start_date','end_date', 'start_mileage']
 
     def validate(self,data):
+        user = self.context['request'].user
+
         try: 
             instance = Rental(**data)
             instance.full_clean()
