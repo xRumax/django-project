e7b6006c4895c5d5c5fbf592109a6b750d11f21d
xRumax <ruminski.dawid77@gmail.com>
2026-04-22 22:54:52 +0200

    feat: secure Rental API with user-specific filtering and custom 'my' action

--- backend/rentals/views/rental_views.py
+++ backend/rentals/views/rental_views.py
@@ -2,19 +2,18 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, BasePermission
 from rentals.models.rental import Rental
 from rentals.serializers.rental_serializer import RentalSerializer, CreateRentalSerializer
+from common.pagination import CustomPagination
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-class isOwner(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj.user.id == request.user.id
 class RentalViewSet(ModelViewSet):
     serializer_class = RentalSerializer
     queryset = Rental.objects.all().order_by('id')
+    pagination_class = CustomPagination
 
     def get_permissions(self):
-        if self.action in ['list', 'update', 'partial_update', 'destroy']:
+        if self.action in ['list', 'update', 'partial_update', 'destroy', 'retrieve']:
             return [IsAdminUser()]
-        if self.action in ['retrieve']:
-            return [isOwner()]
         return [IsAuthenticated()]
     
     def get_serializer_class(self):
@@ -28,8 +27,13 @@ class RentalViewSet(ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
-
         if user.is_staff:
             return Rental.objects.all().order_by('id')
         return Rental.objects.filter(user = user).order_by('id')
+    
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def my(self, request):       
+        rentals = self.get_queryset() 
+        serializer = RentalSerializer(rentals, many = True)
+        return Response(serializer.data)
     
